f74784377374ec68243a155c974381eaa9f5842d
Barbara Viana <vianabsouza@gmail.com>
2024-09-06 17:32:23 -0300

    feat: add html and js files for second dom manipulation exercise

---
+++ javascript/manipulacao-dom-II/script.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body");
+
+body.innerHTML = `
+  <h1>Harry Potter Books</h1>
+
+  <h2>Harry Potter and the Philosopher's Stone</h2>
+  <p>Harry discovers he's a wizard and begins his journey at Hogwarts School of Witchcraft and Wizardry.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Chamber of Secrets</h2>
+  <p>Harry returns to Hogwarts and faces a mysterious threat in the school.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Prisoner of Azkaban</h2>
+  <p>Harry learns more about his past and confronts a fugitive prisoner.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Goblet of Fire</h2>
+  <p>Harry competes in a dangerous magical tournament and faces dark forces.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Order of the Phoenix</h2>
+  <p>Harry and his friends fight against a rising threat and deal with a government in denial.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Half-Blood Prince</h2>
+  <p>Harry delves into Voldemort's past and prepares for the final battle.</p>
+  <p>Price: $20</p>
+
+  <h2>Harry Potter and the Deathly Hallows</h2>
+  <p>Harry, Ron, and Hermione search for and destroy Voldemort's Horcruxes and face the final battle.</p>
+  <p>Price: $20</p>
+`
