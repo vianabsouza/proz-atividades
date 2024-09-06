3894ce7b2467841067e7f69747fddedcc767ea1e
Barbara Viana <vianabsouza@gmail.com>
2024-09-06 14:29:13 -0300

    chore: fix indentation

--- javascript/template-strings/script.js
+++ javascript/template-strings/script.js
@@ -1,11 +1,12 @@
 let operacoes = (num1, num2) => {
   return (
-   `
-    ${num1} + ${num2} = ${num1 + num2}
-    ${num1} - ${num2} = ${num1 - num2}
-    ${num1} * ${num2} = ${num1 * num2}
-    ${num1} / ${num2} = ${num1 / num2}
-  `)
+    `
+      ${num1} + ${num2} = ${num1 + num2}
+      ${num1} - ${num2} = ${num1 - num2}
+      ${num1} * ${num2} = ${num1 * num2}
+      ${num1} / ${num2} = ${num1 / num2}
+    `
+  )
 }
 
 console.log(operacoes(5, 5));
