232f2b8d3178cecf566435dca2e15815ac88f53c
Barbara Viana <vianabsouza@gmail.com>
2024-09-06 14:25:29 -0300

    feat: add script for template strings exercise

---
+++ javascript/template-strings/script.js
@@ -0,0 +1,11 @@
+let operacoes = (num1, num2) => {
+  return (
+   `
+    ${num1} + ${num2} = ${num1 + num2}
+    ${num1} - ${num2} = ${num1 - num2}
+    ${num1} * ${num2} = ${num1 * num2}
+    ${num1} / ${num2} = ${num1 / num2}
+  `)
+}
+
+console.log(operacoes(5, 5));
